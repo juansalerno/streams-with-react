9036d43d12d07f5516cf019b5eb43d6cec5eba4f
juansalerno <salerno_juanig@hotmail.com>
2023-11-10 12:34:18 -0300

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const el = document.querySelector('#root');
+const root = ReactDOM.createRoot(el);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+   reducers,
+   composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+root.render(
+   <Provider store={store}>
+      <App />
+   </Provider>
+);
